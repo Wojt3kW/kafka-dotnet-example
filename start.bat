80af3bb5b0db2bba43075680004b91f6e26fa24a
Wojt3kW <wojtek.wojdylo@wp.pl>
2023-10-25 11:15:41 +0200

    added checking if docker desktop is running

--- start.bat
+++ start.bat
@@ -1,4 +1,6 @@
- @rem Check administrative permissions using net session command.
+@echo off
+ 
+@rem Check administrative permissions using net session command.
 net session >nul 2>&1
 if %errorlevel% == 0 (
     echo Successfully checked administrative permissions.
@@ -7,4 +9,16 @@ if %errorlevel% == 0 (
     exit /b 1
 )
 
+@rem Check if Docker desktop is running
+docker ps 2>&1 | find /I /C "error" > NUL
+if %errorlevel% EQU 0 (
+    ECHO IMPORTANT: Run DOCKER DESKTOP before you start this script!
+    ECHO IMPORTANT: I wait 30 seconds for DOCKER DESKTOP to launch, then continue.
+    timeout 30
+)
+
+echo Running docker compose files.
+
 docker-compose -f kafka-docker-compose.yaml up
+
+echo Done!
