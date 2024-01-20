5b138addf5049e0bbc762c5c3a4c0bf0928417c9
Wojt3kW <wojtek.wojdylo@wp.pl>
2024-01-20 14:14:30 +0100

    extended start script added creation of folders in which data are stored

--- start.bat
+++ start.bat
@@ -9,6 +9,34 @@ if %errorlevel% == 0 (
     exit /b 1
 )
 
+setlocal EnableDelayedExpansion
+
+@rem Create directory where zookeeper data will be stored
+@rem If you change this directory remember to set same path at .env file at ZOOKEEPER_DATA_DIR
+if not exist "c:\kafka-demo\zookeeper" (
+  mkdir "c:\kafka-demo\zookeeper"
+  if "!errorlevel!" EQU "0" (
+    echo Successfully created folder "c:\kafka-demo\zookeeper".
+  ) else (
+    echo Error while creating folder "c:\kafka-demo\zookeeper".
+    exit /b %errorlevel%
+  )
+)
+
+@rem Create directory where kafka data will be stored
+@rem If you change this directory remember to set same path at .env file at KIBANA_DATA_DIR
+if not exist "c:\kafka-demo\kafka" (
+  mkdir "c:\kafka-demo\kafka"
+  if "!errorlevel!" EQU "0" (
+    echo Successfully created folder "c:\kafka-demo\kafka".
+  ) else (
+    echo Error while creating folder "c:\kafka-demo\kafka".
+    exit /b %errorlevel%
+  )
+)
+
+setlocal DisableDelayedExpansion
+
 @rem Check if Docker desktop is running
 docker ps 2>&1 | find /I /C "error" > NUL
 if %errorlevel% EQU 0 (
